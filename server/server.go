487fe0b2bcf851481daabbc201ba9c547e47a0a3
sky0621 <sky14work@gmail.com>
2019-10-11 09:12:54 +0900

    gqlerror

--- server/server.go
+++ server/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/gqlerror"
 
 	"github.com/99designs/gqlgen/handler"
 	study_gqlgen "github.com/sky0621/study-gqlgen"
@@ -18,7 +22,28 @@ func main() {
 	}
 
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(study_gqlgen.NewExecutableSchema(study_gqlgen.Config{Resolvers: &study_gqlgen.Resolver{}})))
+	http.Handle("/query", handler.GraphQL(
+		study_gqlgen.NewExecutableSchema(study_gqlgen.Config{Resolvers: &study_gqlgen.Resolver{}}),
+		handler.ErrorPresenter(
+			func(ctx context.Context, e error) *gqlerror.Error {
+				//if myError, ok := e.(MyError); ok {
+				//	return gqlerror.ErrorPathf(graphql.GetResolverContext(ctx).Path(), "Eeek!")
+				//}
+
+				return graphql.DefaultErrorPresenter(ctx, e)
+			},
+		),
+		handler.RequestMiddleware(
+			func(ctx context.Context, next func(ctx context.Context) []byte) []byte {
+				return next(ctx)
+			},
+		),
+		handler.ResolverMiddleware(
+			func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+				return next(ctx)
+			},
+		),
+	))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
